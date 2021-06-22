bdc244b38bedaff5f7d0726aeed62cf67337b6cd
Ingo Köpp <ingokpp@gmail.com>
2021-06-22 21:18:44 +0200

    feat: add second version of header + navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -16,26 +15,6 @@ export default function Home() {
             </Head>
 
             <Header></Header>
-
-            <main>hello world</main>
-
-            <footer>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{" "}
-                    <span>
-                        <Image
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            width={72}
-                            height={16}
-                        />
-                    </span>
-                </a>
-            </footer>
         </div>
     );
 }
